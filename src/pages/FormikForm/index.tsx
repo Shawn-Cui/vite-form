ddedbf7a297132cd22a80d386c794a98ef6455f3
cuitao <cui.tao@xinheyun.com>
2021-03-02 18:09:36 +0800

    feat: replace useFormik with Formik component

--- src/pages/FormikForm/index.tsx
+++ src/pages/FormikForm/index.tsx
@@ -4,66 +4,71 @@
  * Formik 版本
  */
 import React from 'react';
-import { useFormik } from 'formik';
+import { Formik } from 'formik';
 import * as Yup from 'yup';
 
 const FormikForm = () => {
-  const formik = useFormik({
-    initialValues: {
-      firstName: '',
-      lastName: '',
-      email: '',
-    },
-    validationSchema: Yup.object({
-      firstName: Yup.string()
-        .max(15, '最多只能填写15个字符')
-        .required('必填'),
-      lastName: Yup.string()
-        .max(20, '最多只能填写20个字符')
-        .required('必填'),
-      email: Yup.string()
-        .email('不合法的邮箱格式')
-        .required('必填'),
-    }),
-    onSubmit: values => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
-
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="firstName">姓氏</label>
-      <input
-        id="firstName"
-        type="text"
-        {...formik.getFieldProps('firstName')}
-      />
-      {formik.touched.firstName && formik.errors.firstName ? (
-        <div>{formik.errors.firstName}</div>
-      ) : null}
+    <Formik
+      initialValues={{
+        firstName: '',
+        lastName: '',
+        email: '',
+      }}
+      validationSchema={Yup.object({
+        firstName: Yup.string()
+          .max(15, '最多只能填写15个字符')
+          .required('必填'),
+        lastName: Yup.string()
+          .max(20, '最多只能填写20个字符')
+          .required('必填'),
+        email: Yup.string()
+          .email('不合法的邮箱格式')
+          .required('必填'),
+      })}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        });
+      }}
+    >
+      {formik => (
+        <form onSubmit={formik.handleSubmit}>
+          <label htmlFor="firstName">姓氏</label>
+          <input
+            id="firstName"
+            type="text"
+            {...formik.getFieldProps('firstName')}
+          />
+          {formik.touched.firstName && formik.errors.firstName ? (
+            <div>{formik.errors.firstName}</div>
+          ) : null}
 
-      <label htmlFor="lastName">名称</label>
-      <input
-        id="lastName"
-        type="text"
-        {...formik.getFieldProps('lastName')}
-      />
-      {formik.touched.lastName && formik.errors.lastName ? (
-        <div>{formik.errors.lastName}</div>
-      ) : null}
+          <label htmlFor="lastName">名称</label>
+          <input
+            id="lastName"
+            type="text"
+            {...formik.getFieldProps('lastName')}
+          />
+          {formik.touched.lastName && formik.errors.lastName ? (
+            <div>{formik.errors.lastName}</div>
+          ) : null}
 
-      <label htmlFor="email">邮箱</label>
-      <input
-        id="email"
-        type="email"
-        {...formik.getFieldProps('email')}
-      />
-      {formik.touched.email && formik.errors.email ? (
-        <div>{formik.errors.email}</div>
-      ) : null}
+          <label htmlFor="email">邮箱</label>
+          <input
+            id="email"
+            type="email"
+            {...formik.getFieldProps('email')}
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <div>{formik.errors.email}</div>
+          ) : null}
 
-      <button type="submit">提交</button>
-    </form>
+          <button type="submit">提交</button>
+        </form>
+      )}
+    </Formik>
   );
 };
 
